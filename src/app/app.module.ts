f9adcb984af8294df8887c24c2e1ca3cb672ba4d
hansajayathilaka <hansajayathilaka@yahoo.com>
2019-12-09 09:33:56 +0530

    Create Service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule
